7b15446d6e2ec640623a8a0ac4b0cd983dc4c3ec
tandavala <jose.tandavala@gmail.com>
2018-12-05 17:03:39 +0100

    programando a funcao gestao de disciplinas

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from turmas.models import Turmas
 
-# Create your models here.
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=255)
+    estudante = models.ManyToManyField(User, blank=True)
+    turma = models.ManyToManyField(Turmas, blank=True)
+    nota = models.FloatField(default=0, blank=True)
+
+    class Meta:
+        verbose_name = "disciplina"
+        verbose_name_plural = "disciplinas"
+
+    def __str__(self):
+        return self.nome

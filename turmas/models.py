ba2210deb54ecc133446a7c99d4c8c38edf0eece
tandavala <jose.tandavala@gmail.com>
2018-12-06 16:53:55 +0100

    refactorando turmas

--- turmas/models.py
+++ turmas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cursos.models import Cursos
+from disciplinas.models import Disciplina
 
 class Turmas(models.Model):
 
@@ -22,7 +23,8 @@ class Turmas(models.Model):
     estudante = models.ManyToManyField(User, blank=True)
     responsavel = models.IntegerField(default= 0, null = True, blank=True)
     periodo = models.CharField('Tipo de peerfil', max_length=1, choices=PERI, default=ESCOLHER,  blank=True, null=True)
-
+    disciplina = models.ManyToManyField(Disciplina, blank=True)
+    
     class Meta:
         verbose_name = "Turma"
         verbose_name_plural = "turmas"

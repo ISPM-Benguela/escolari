71de7ff7482723477cf800eaecaf6e0a40166af0
tandavala <jose.tandavala@gmail.com>
2018-12-08 18:36:32 +0100

    adiconar analise de dados no painel frontal do painel de controle

--- kernel/views.py
+++ kernel/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from  django.contrib.auth.models import User
 from departamentos.models import Departamentos
+from salas.models import Turmas
 
 def inicio(request):
     if request.user.is_authenticated():
         return render(request, 'painel/index.html', {
 
         'departamentos': Departamentos.objects.all(),
+        'departNo' : Departamentos.objects.all().count(),
+        'turmaNo' : Turmas.objects.all().count(),
         })
     return render(request,'kernel/inicio.html')

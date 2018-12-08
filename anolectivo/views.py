d42736bfdd29e0e6965f71db21f23240cea7fe19
tandavala <jose.tandavala@gmail.com>
2018-12-08 17:30:59 +0100

    refatorando o codigo

--- anolectivo/views.py
+++ anolectivo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def todos(request):
+    
+
+    return render(request, 'anolectivo/index.html')
--- finallapp/urls.py
+++ finallapp/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^turmas/', include('salas.urls')),
     url(r'^cursos/', include('cursos.urls')),
     url(r'^eventos/', include('eventos.urls')),
+    url(r'^ano/', include('anolectivo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 

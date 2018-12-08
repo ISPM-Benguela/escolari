acebd1a97e7fa017ca131eb240eeef6ec6eedf3d
tandavala <jose.tandavala@gmail.com>
2018-12-08 20:59:38 +0100

    criando evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -4,7 +4,7 @@ from eventos import views as apresenta
 
 urlpatterns = [  
     url(r'^$', apresenta.todos, name="eventos"),
-    #url(r'^cadastrar/', apresenta.cadastrar_funcionario , name="cadastrafuncionario"),
+     url(r'^cadastrar/', apresenta.cadastrar_evento, name="cadastraevento"),
     #url(r'^editar/(?P<nome>[-\w]+)/$', apresenta.editar_funcionario , name="editarusuario"),
     # url(r'^actualizar/(?P<nome>[-\w]+)/$', apresenta.editar_funcionario , name="editarfuncionario"),
     #url(r'^(?P<funcionario>\w+)', apresenta.sozinho, name="single"),
--- eventos/views.py
+++ eventos/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,  HttpResponse
 from departamentos.models import Departamentos
 from eventos.models import Eventos
+from django.shortcuts import redirect
+from eventos.forms import EventoForm
 
 def todos(request):
     contexto = {
         'departamentos': Departamentos.objects.all(),
         'eventos' : Eventos.objects.all(),
+        'form' : EventoForm,
     }
     return render(request, 'eventos/index.html', contexto)
+
+def cadastrar_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    return redirect('/eventos')

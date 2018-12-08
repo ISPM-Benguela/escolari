bcb01eb371b1fccc91ae8c8864afea48eee4dbe3
tandavala <jose.tandavala@gmail.com>
2018-12-08 19:48:37 +0100

    adicionando analise de dado no painel de controle

--- funcionarios/views.py
+++ funcionarios/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render , HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from  django.contrib.auth.models import User
+from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from departamentos.models import Departamentos
 from funcionarios.forms import FuncionarioForm
+from usuarios.models import Perfil
+
 
 
 # Create your views here.
 def todos(request):
+    queryset = Perfil.objects.filter(
+        Q(tipo_perfil__startswith='F')
+    )
     return render(request, 'funcionarios/index.html', {
-        'funcionarios' : User.objects.all(),
+        'funcionarios' : queryset,
         'form' : UserCreationForm,
         'departamentos' : Departamentos.objects.all(),
     })
--- kernel/views.py
+++ kernel/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from  django.contrib.auth.models import User
+from django.db.models import Q
 from departamentos.models import Departamentos
 from salas.models import Turmas
+from usuarios.models import Perfil
 
 def inicio(request):
+    queryset = Perfil.objects.filter(
+        Q(tipo_perfil__startswith='E')
+    )
+    contaEstudante = Perfil.objects.filter(
+        Q(tipo_perfil__startswith='E')
+    ).count()
+
+    contaFuncionario = Perfil.objects.filter(
+        Q(tipo_perfil__startswith='F')
+    ).count()
+
+    
     if request.user.is_authenticated():
         return render(request, 'painel/index.html', {
 
         'departamentos': Departamentos.objects.all(),
         'departNo' : Departamentos.objects.all().count(),
         'turmaNo' : Turmas.objects.all().count(),
+        'estudanteNo' : contaEstudante,
+        'funcionarioNo' : contaFuncionario,
         })
     return render(request,'kernel/inicio.html')
+    

d2b1b964a65f06a40b43f5f28d56b04cc8e84091
tandavala <jose.tandavala@gmail.com>
2018-12-09 19:38:01 +0100

    adicionando tratamento de errors

--- eventos/views.py
+++ eventos/views.py
@@ -5,10 +5,16 @@ from django.shortcuts import redirect
 from eventos.forms import EventoForm
 
 def todos(request):
+    mensagens = ['temos' , 'outro qui','mais outro']
+    mensagens.append('novo item agora')
     contexto = {
         'departamentos': Departamentos.objects.all(),
         'eventos' : Eventos.objects.all(),
         'form' : EventoForm,
+        'mensagens' : mensagens,
+        'error' : False,
+        'sucesso': True,
+
     }
     return render(request, 'eventos/index.html', contexto)
 

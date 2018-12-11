db2925e748e2841394fb8b7df72150ba66349c6c
tandavala <jose.tandavala@gmail.com>
2018-12-11 08:08:38 +0100

    adicionado o formulario na visao da lista dos estudantes

--- estudantes/views.py
+++ estudantes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from departamentos.models import Departamentos
+from estudantes.forms import EstudanteForm
 
 def inicio(request):
     contexto = {
         'departamentos' : Departamentos.objects.all(),
+        'form' : EstudanteForm,
     }
     return render(request, 'estudantes/inicio.html', contexto)

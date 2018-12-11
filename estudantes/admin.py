60b7d2fbd97239fb6cdc2e6a9be23684fdf14472
tandavala <jose.tandavala@gmail.com>
2018-12-11 07:56:38 +0100

    programando o modelo estudante e adicionando o panel de controle do django

--- estudantes/admin.py
+++ estudantes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from estudantes.models import Estudantes
 
-# Register your models here.
+admin.site.register(Estudantes)
--- estudantes/models.py
+++ estudantes/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from salas.models import Turmas
 
 class Estudantes(models.Model):
-    pass 
+    user = models.ForeignKey(User)
+    turma = models.ForeignKey(Turmas, blank=True, null=True)
+    nome = models.CharField(max_length=255, blank=True, null=True)
 
-    class Media:
-        css = {
-            "all" : ("css/estudante.css",)
-        }
-        js = ('js/estudante.js',)
+    class Meta:
+        verbose_name = "Estudante"
+        verbose_name_plural = "Estudantes"
+    
+    def __str__(self):
+        return self.nome 
+
+    

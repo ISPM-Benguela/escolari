aed04c5da19fd2f8acde9d4a467308645a1d42e1
tandavala <jose.tandavala@gmail.com>
2018-12-12 11:29:23 +0100

    trazendo django form na visao (html)

--- salas/views.py
+++ salas/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from departamentos.models import Departamentos
 from salas.models import Turmas
+from salas.forms import TurmaForm
 
 def todos(request):
     turmas = Turmas.objects.all()
     return render(request, 'turmas/index.html', {
         'departamentos': Departamentos.objects.all(),
         'turmas' : turmas,
+        'form' : TurmaForm,
     })
 
 def cadastrar_turma(request):

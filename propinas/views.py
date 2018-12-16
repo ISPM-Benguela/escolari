12a21aea227319782ecb39a6916cf52d93f1c281
tandavala <jose.tandavala@gmail.com>
2018-12-16 18:51:23 +0100

    pegando os dados do estudante na base de dados

--- propinas/views.py
+++ propinas/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from departamentos.models import Departamentos
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from meses.models import Meses
+from anolectivo.models import AnoLectivo
 
 def inicio(request):
     return render(request, 'propinas/inicio.html',{
@@ -7,4 +11,11 @@ def inicio(request):
     })
 
 def pagar_propinas(request, nome):
-    return HttpResponse(nome)
+    estudante = get_object_or_404(User, username=nome)
+
+    return render(request, 'propinas/pagamentos.html',{
+        'departamentos' : Departamentos.objects.all(),
+        'estudante' : estudante,
+        'meses': Meses.objects.all(),
+        'anolectivo': AnoLectivo.objects.all(),
+    })

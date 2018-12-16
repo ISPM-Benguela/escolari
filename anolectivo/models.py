d9461084bc8af8b70607d908875888c16e19d7fa
tandavala <jose.tandavala@gmail.com>
2018-12-16 21:45:54 +0100

    ordenando os meses

--- anolectivo/models.py
+++ anolectivo/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+import datetime
 
 class AnoLectivo(models.Model):
     ano = models.DateField()
+    criado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Ano Lectivo"
         verbose_name_plural = "Anos LEctivos"
+        ordering = ['criado']
     
     def __str__(self):
         return "%s" % self.ano
--- meses/models.py
+++ meses/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+import datetime
 
 class Meses(models.Model):
     mes = models.CharField(max_length=255)
+    criado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('mes',)
         verbose_name = "Mes"
         verbose_name_plural = "Meses"
+        ordering = ['criado']
 
     def __str__(self):
         return self.mes

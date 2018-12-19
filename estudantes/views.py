d4b2a68de666fad354ef1b757ff6db9b26587ffe
tandavala <jose.tandavala@gmail.com>
2018-12-20 00:02:19 +0100

    adicionando o ntificao no menu com estudantes

--- estudantes/views.py
+++ estudantes/views.py
@@ -5,6 +5,8 @@ from departamentos.models import Departamentos
 from estudantes.forms import EstudanteForm
 from usuarios.models import Perfil
 from propinas.models import Propinas
+from candidato.models import Candidato
+from mensagem.models import Mensagem
 
 def inicio(request):
     queryset = Perfil.objects.filter(tipo_perfil='E')
@@ -15,5 +17,10 @@ def inicio(request):
         'estudantes': queryset,
         'date_actual' : datetime.date.today(),
         'ultima_propina' : Propinas.objects.last(),
+        
+        'feed': Mensagem.objects.filter(por_ler=True).count(),
+        'mensagens': Mensagem.objects.filter(por_ler=True),
+        'feedcandidato': Candidato.objects.filter(novo=True).count(),
+        'candidatos': Candidato.objects.filter(novo=True),
     }
     return render(request, 'estudantes/inicio.html', contexto)
--- salas/views.py
+++ salas/views.py
@@ -8,9 +8,11 @@ from mensagem.models import Mensagem
 from candidato.models import Candidato
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login , authenticate
+from django.contrib.auth.models import User
 from usuarios.forms import EstudanteForm
 from candidato.models import Candidato
 from mensagem.models import Mensagem
+from usuarios.models import Perfil
 
 def todos(request):
     turmas = Turmas.objects.all()
@@ -22,17 +24,25 @@ def todos(request):
         'mensagens': Mensagem.objects.filter(por_ler=True),
         'feedcandidato': Candidato.objects.filter(novo=True).count(),
         'candidatos': Candidato.objects.filter(novo=True),
+        'userform': UserCreationForm,
     })
 
 def cadastrar_turma(request):
+
+    senha = '1cristo2bom3'
+
     if request.method == 'POST':
         _nome = request.POST.get('nome')
         _sobrenome = request.POST.get('sobrenome')
         _turma = request.POST.get('turma')
         _email = request.POST.get('email')
 
+        user = User(username=_nome)
+        user.set_password(senha)
+        user.save()
 
-        return HttpResponse("%s %s %s %s" % (_nome, _sobrenome, _email, _turma))
+        if user:
+            return HttpResponse(user.perfil.turma.get_turma)
     return HttpResponse("nao")
     
 

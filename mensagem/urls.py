142385934c3b3490a8a92a6b39f10b8e9694cfcb
tandavala <jose.tandavala@gmail.com>
2018-12-19 18:57:53 +0100

    enviando mensagem apartir da pagina clienre

---
+++ mensagem/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url 
+from mensagem import views as visao
+
+urlpatterns = [
+
+    url(r'^$', visao.inicio, name="mensagem"),
+   # url(r'^cadastrar/', visao.cadastrar_anolectivo , name="cadastranolectivo"),
+    #url(r'^editar/(?P<nome>[-\w]+)/$', visao.editar_anolectivo , name="editarano"),
+   # url(r'^eliminar/(?P<nome>[-\w]+)/$', visao.eliminar_anolectivo , name="eliminarano"),
+   # url(r'^actualizar/(?P<num>[0-9]+)/$', visao.actualizar_anolectivo , name="actualizaranolectivo"),
+]
--- mensagem/views.py
+++ mensagem/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from mensagem.models import Mensagem
 
-# Create your views here.
+def inicio(request):
+    
+
+    if request.method == 'POST':
+        _nome = request.POST.get('nome','')
+        _email = request.POST.get('email','')
+        _telefone = request.POST.get('telefone','')
+        _assunto = request.POST.get('assunto','')
+        _mensagem = request.POST.get('mensagem')
+
+        confirmar = Mensagem(
+            nome=_nome,
+            email=_email,
+            telefone=_telefone,
+            assunto = _assunto,
+            mensagem=_mensagem
+        )
+        confirmar.save()
+
+        if confirmar:
+            return HttpResponse("mensagrm enviada")
+        else:
+            return HttpResponse("nao enviada")
+
+
+
+    return HttpResponse("nao guarda este dados nao e segura tira fora este usuario mau")

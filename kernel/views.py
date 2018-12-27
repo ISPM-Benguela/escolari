a8c1d67ec61cfa12dc3cdf9f621a5a75653b65f7
tandavala <jose.tandavala@gmail.com>
2018-12-27 15:02:05 +0100

    separando a pagina cliente do painel de contrle

--- kernel/views.py
+++ kernel/views.py
@@ -20,20 +20,6 @@ def inicio(request):
         Q(tipo_perfil__startswith='F') |  Q(tipo_perfil__startswith='P')
     ).count()
 
-    
-    if request.user.is_authenticated():
-        return render(request, 'painel/index.html', {
-
-        'departamentos': Departamentos.objects.all(),
-        'departNo' : Departamentos.objects.all().count(),
-        'turmaNo' : Turmas.objects.all().count(),
-        'estudanteNo' : contaEstudante,
-        'funcionarioNo' : contaFuncionario,
-        'feed': Mensagem.objects.filter(por_ler=True).count(),
-        'mensagens': Mensagem.objects.filter(por_ler=True),
-        'feedcandidato': Candidato.objects.filter(novo=True).count(),
-        'candidatos': Candidato.objects.filter(novo=True),
-        })
     return render(request,'kernel/inicio.html',{
 
         'departNo' : Departamentos.objects.all().count(),

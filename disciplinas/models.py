753806a576d72beff42a6ff7de4ff10233682a7a
tandavala <jose.tandavala@gmail.com>
2019-01-05 12:45:03 +0100

    criando migracao

--- disciplinas/models.py
+++ disciplinas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User 
-
+from cursos.models import Cursos
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=255)
     estudante = models.ManyToManyField(User, blank=True)
+    cursos = models.ForeignKey(Cursos, blank=True, null=True)
     nota = models.FloatField(default=0, blank=True)
 
     class Meta:
